b2b967607eb5aeb26eb1290d5019b6a45b248b22
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-26 15:01:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(void){
 	Struct_PWM PWM_Init_Struct;
 	
 	PWM_Init_Struct.Waveform_Generation_Mode = Fast_PWM;
-	PWM_Init_Struct.Compare_Output_Mode		 = WGM_Fast_PWM__Non_Invertin_Mode;
-	PWM_Init_Struct.Prescaler_Select		 = Division_Factor_1024;
+	PWM_Init_Struct.Compare_Output_Mode	 = WGM_Fast_PWM__Non_Invertin_Mode;
+	PWM_Init_Struct.Prescaler_Select	 = Division_Factor_1024;
 	
 	PWM_Init(PWM_Init_Struct);
 
