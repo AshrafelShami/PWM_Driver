08865ea9c67a5427352cf20f466051b21cbd7990
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-26 15:01:01 +0200

    Update PWM_Config.c

--- PWM_Config.c
+++ PWM_Config.c
@@ -13,40 +13,40 @@
 void PWM_Init( Struct_PWM PWM_Config ){
 	DDRB |= (1 << 3);		/*	Set OC0 pin as output	*/
 	switch (PWM_Config.Waveform_Generation_Mode){
-		case Normal				: TCCR0 &=~(1 << WGM01); TCCR0 &=~(1 << WGM00); break;
+		case Normal		: TCCR0 &=~(1 << WGM01); TCCR0 &=~(1 << WGM00); break;
 		case PWM_Phase_Correct	: TCCR0 &=~(1 << WGM01); TCCR0 |= (1 << WGM00); break;
-		case CTC				: TCCR0 |= (1 << WGM01); TCCR0 &=~(1 << WGM00); break;
-		case Fast_PWM			: TCCR0 |= (1 << WGM01); TCCR0 |= (1 << WGM00); break;
-		default					: TCCR0 |= (1 << WGM01); TCCR0 |= (1 << WGM00); break;
+		case CTC		: TCCR0 |= (1 << WGM01); TCCR0 &=~(1 << WGM00); break;
+		case Fast_PWM		: TCCR0 |= (1 << WGM01); TCCR0 |= (1 << WGM00); break;
+		default			: TCCR0 |= (1 << WGM01); TCCR0 |= (1 << WGM00); break;
 	}
 	
 	switch (PWM_Config.Compare_Output_Mode){
-		case WGM_Normal__Normal_Port_Operation			: TCCR0 &=~ (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Normal__Toggle_On_CMP_Match			: TCCR0 &=~ (1 << COM01); TCCR0 |=  (1 << COM00); break;
-		case WGM_Normal__Clear_On_CMP_Match				: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Normal__Set_On_CMP_Match				: TCCR0 |=  (1 << COM01); TCCR0 |=	(1 << COM00); break;
+		case WGM_Normal__Normal_Port_Operation	: TCCR0 &=~ (1 << COM01); TCCR0 &=~ (1 << COM00); break;
+		case WGM_Normal__Toggle_On_CMP_Match	: TCCR0 &=~ (1 << COM01); TCCR0 |=  (1 << COM00); break;
+		case WGM_Normal__Clear_On_CMP_Match	: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
+		case WGM_Normal__Set_On_CMP_Match	: TCCR0 |=  (1 << COM01); TCCR0 |=  (1 << COM00); break;
 		
-		case WGM_Fast_PWM__Normal_Port_Operation		: TCCR0 &=~ (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Fast_PWM__Non_Invertin_Mode			: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Fast_PWM__Inverting_Mode				: TCCR0 |=  (1 << COM01); TCCR0 |=	(1 << COM00); break;
+		case WGM_Fast_PWM__Normal_Port_Operation: TCCR0 &=~ (1 << COM01); TCCR0 &=~ (1 << COM00); break;
+		case WGM_Fast_PWM__Non_Invertin_Mode	: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
+		case WGM_Fast_PWM__Inverting_Mode	: TCCR0 |=  (1 << COM01); TCCR0 |=  (1 << COM00); break;
 		
 		case WGM_Phase_Correct__Normal_Port_Operation	: TCCR0 &=~ (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Phase_Correct__Non_Invertin_Mode		: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
-		case WGM_Phase_Correct__Inverting_Mode			: TCCR0 |=  (1 << COM01); TCCR0 |=	(1 << COM00); break;
+		case WGM_Phase_Correct__Non_Invertin_Mode	: TCCR0 |=  (1 << COM01); TCCR0 &=~ (1 << COM00); break;
+		case WGM_Phase_Correct__Inverting_Mode		: TCCR0 |=  (1 << COM01); TCCR0 |=  (1 << COM00); break;
 		
-		default											: TCCR0 |=  (1 << COM01); TCCR0 |=	(1 << COM00); break;
+		default						: TCCR0 |=  (1 << COM01); TCCR0 |=  (1 << COM00); break;
 	}
 
 	switch(PWM_Config.Prescaler_Select){
-		case No_Clock_Source				: TCCR0 &=~(1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 &=~(1 << CS00); break;
-		case Division_Factor_OFF			: TCCR0 &=~(1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
-		case Division_Factor_8				: TCCR0 &=~(1 << CS02); TCCR0 |= (1 << CS01); TCCR0 &=~(1 << CS00); break;
-		case Division_Factor_64				: TCCR0 &=~(1 << CS02); TCCR0 |= (1 << CS01); TCCR0 |= (1 << CS00); break;
-		case Division_Factor_256			: TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 &=~(1 << CS00); break;
-		case Division_Factor_1024			: TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
-		case External_Clk_Src_Falling_egde	: TCCR0 |= (1 << CS02); TCCR0 |= (1 << CS01); TCCR0 &=~(1 << CS00); break;
-		case External_Clk_Src_Rising_egde	: TCCR0 |= (1 << CS02); TCCR0 |= (1 << CS01); TCCR0 |= (1 << CS00); break;
-		default								: TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
+		case No_Clock_Source		  : TCCR0 &=~(1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 &=~(1 << CS00); break;
+		case Division_Factor_OFF	  : TCCR0 &=~(1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
+		case Division_Factor_8		  : TCCR0 &=~(1 << CS02); TCCR0 |= (1 << CS01); TCCR0 &=~(1 << CS00); break;
+		case Division_Factor_64		  : TCCR0 &=~(1 << CS02); TCCR0 |= (1 << CS01); TCCR0 |= (1 << CS00); break;
+		case Division_Factor_256	  : TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 &=~(1 << CS00); break;
+		case Division_Factor_1024	  : TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
+		case External_Clk_Src_Falling_egde: TCCR0 |= (1 << CS02); TCCR0 |= (1 << CS01); TCCR0 &=~(1 << CS00); break;
+		case External_Clk_Src_Rising_egde : TCCR0 |= (1 << CS02); TCCR0 |= (1 << CS01); TCCR0 |= (1 << CS00); break;
+		default				  : TCCR0 |= (1 << CS02); TCCR0 &=~(1 << CS01); TCCR0 |= (1 << CS00); break;
 	}
 }
 
@@ -54,7 +54,7 @@ void PWM_Init( Struct_PWM PWM_Config ){
   Name:     PWM_Duty_Cycle.
   Purpose:  Initialization for Timer0/PWM Registers.
   Entry:    (u8 Duty_Cycle): Function Takes the DutyCycle (0 ~ 100),
-							 and set the Output Compare register.
+			     and set the Output Compare register.
   Exit:     no parameters
 ********************************************************************************************/
 void PWM_Duty_Cycle ( u8 Duty_Cycle ){
